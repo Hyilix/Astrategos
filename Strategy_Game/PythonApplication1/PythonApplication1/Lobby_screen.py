1d23e4b839033ffdf6187a52ee70e76702f48b04
Sorint <pssorin13@gmail.com>
2022-09-22 20:31:25 +0300

    draw name

--- Strategy_Game/PythonApplication1/PythonApplication1/Lobby_screen.py
+++ Strategy_Game/PythonApplication1/PythonApplication1/Lobby_screen.py
@@ -4,13 +4,15 @@ import socket
 import pickle
 import threading
 
+White = (255,255,255)
 
 def lobby(WIN,WIDTH,HEIGHT,FPS,Role,name,Connection) :
     pygame.init()
 
-    playeri = [["",0],["",0],["",0],["",0]]
-
+    playeri = [["",None,0],["",None,0],["",None,0],["",None,0]]
+    Font = pygame.font.Font(None, 40)
     Cerc_draw = []
+    Text_draw = []
 
     y = HEIGHT/2
     diametru = (WIDTH - 50*3)/6
@@ -20,14 +22,22 @@ def lobby(WIN,WIDTH,HEIGHT,FPS,Role,name,Connection) :
 
     def draw_window () :
         WIN.fill((255,255,255))
-        for cerc in Cerc_draw :
-            pygame.draw.circle(WIN,(225, 223, 240),cerc,diametru/2)
+        for i in range( len(Cerc_draw)) :
+            pygame.draw.circle(WIN,(225, 223, 240),Cerc_draw[i],diametru/2)
+            if playeri[i][0] != "" :
+                pygame.draw.circle(WIN,White,Cerc_draw[i],diametru/2 - 10)
+                WIN.blit(playeri[i][1],Text_draw[i])
+
         pygame.display.update()
         
 
 
     if Role == "host" :
         playeri[0][0] = name
+        playeri[0][1] = Font.render(playeri[0][0], True, (0,0,0))
+        text_rect = playeri[0][1].get_rect()
+        text_rect.center = (diametru + diametru/2,HEIGHT/2 - diametru/2-30)
+        Text_draw.append(text_rect)
 
     clock = pygame.time.Clock()
     run = True
