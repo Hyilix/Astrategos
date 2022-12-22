f858f9e70a0da2db2a6a871ee58ce10f5824ad97
Sorint <pssorin13@gmail.com>
2022-12-22 08:03:00 +0200

    fixed small bug

--- Strategy_Game/PythonApplication1/PythonApplication1/Gameplay.py
+++ Strategy_Game/PythonApplication1/PythonApplication1/Gameplay.py
@@ -7,6 +7,9 @@ import threading
 
 pygame.init()
 
+HEADERSIZE = 10
+SPACE = "          "
+
 run = True
 
 def gameplay (WIN,WIDTH,HEIGHT,FPS,Role,Connection,playeri,Pozitie,CLIENTS,Coduri_pozitie_client,map_name) :
@@ -19,8 +22,103 @@ def gameplay (WIN,WIDTH,HEIGHT,FPS,Role,Connection,playeri,Pozitie,CLIENTS,Codur
     def draw_window () :
         pygame.display.update()
 
+    #Functia cu care serverul asculta pentru mesajele unui client
+    def reciev_thread_from_client(client,cod) :
+        try :
+            while True :
+                header = client.recv(10)
+                header = header.decode("utf-8")
+                if len(header) != 0 :
+                    data_recv = client.recv(int(header))
+                    data_recv = pickle.loads(data_recv)
+                    #se va proceseaza mesajul de la clinet
+                else :
+                    client.close()
+                    Killed_Clients.append(cod)
+                    break
+        except :
+            client.close()
+            Killed_Clients.append(cod)
+
+    #Functia clientului care asculta pentru mesaje de la server
+    def reciev_thread_from_server(server) :
+        global run
+        try :
+            while True :
+                header = server.recv(10)
+                header = header.decode("utf-8")
+
+                if len(header) != 0 :
+                    data_recv = server.recv(int(header))
+                    data_recv = pickle.loads(data_recv)
+                    if data_recv[0] == "I_died...Fuck_off":
+                        server.close()
+                        run = False
+                        break
+                    else :
+                        Changes_from_server.append(data_recv)
+                else :
+                    server.close()
+                    run = False
+                    break
+        except :
+            server.close()
+            run = False
+
+    # Incarcarea variabilelor necesare rolurilor de host si client
+    if Role == "host" :
+        Client_THREADS = []
+        Killed_Clients = []
+        Transmit_to_all = []
+        #restart listening threads
+        for i in range(len(CLIENTS)) :
+            newthread = threading.Thread(target = reciev_thread_from_client , args =(CLIENTS[i][0],CLIENTS[i][1]))
+            Client_THREADS.append(newthread)
+            Client_THREADS[len(Client_THREADS)-1].start() 
+    else :
+        #restart listenig to the server
+        recv_from_server = threading.Thread(target = reciev_thread_from_server, args = (Connection,))
+        recv_from_server.start()
+        Changes_from_server = []
+
+    clock = pygame.time.Clock()
+    run=True
     while run == True :
+        clock.tick(FPS)
+        #afiseaza totul
         draw_window()
+
+        #se actualizeaza variabilele care au legatura cu comunicarea dintre server si client
+        if Role == "host":
+            # se verifica daca un player sa deconectat 
+            while len(Killed_Clients) > 0 :
+                Transmit_to_all.append((("leftplayer",Coduri_pozitie_client[Killed_Clients[0]] + 1),None))
+                CLIENTS.pop(Coduri_pozitie_client[Killed_Clients[0]])
+                playeri.pop(Coduri_pozitie_client[Killed_Clients[0]] + 1)
+                Client_THREADS[Coduri_pozitie_client[Killed_Clients[0]]].join()
+                Client_THREADS.pop(Coduri_pozitie_client[Killed_Clients[0]])
+                Coduri_pozitie_client.pop(Killed_Clients[0])
+                #reactualizare in dictionarul clientilor si pozitiile lor
+                for i in Coduri_pozitie_client :
+                    if Coduri_pozitie_client[i] > Killed_Clients[0] :
+                        Coduri_pozitie_client[i] -= 1 
+                Killed_Clients.pop(0)
+            #Se verifica daca este ceva de transmis la ceilalti playeri
+            while len(Transmit_to_all) > 0 :
+                data_send = pickle.dumps(Transmit_to_all[0][0])
+                data_send = bytes((SPACE +str(len(data_send)))[-HEADERSIZE:], 'utf-8') + data_send
+                for i in range(len(CLIENTS)) :
+                    if Transmit_to_all[0][1] == None or Coduri_pozitie_client[Transmit_to_all[0][1]] != i  :
+                        CLIENTS[i][0].send(data_send)
+                Transmit_to_all.pop(0)
+        else :
+            #Se verifica daca serverul a trimis lucruri spre acest client
+            while len(Changes_from_server) > 0 :
+                if Changes_from_server[0][0] == "leftplayer" :
+                    playeri.pop(Changes_from_server[0][1])
+                    if Changes_from_server[0][1] < Pozitie :
+                        Pozitie -= 1 
+                Changes_from_server.pop(0)
 
     #finalul functiei si returnarea variabilelor necesare care s-ar fi putut schimba
     if Role == "host" :
